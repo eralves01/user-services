ffaa37651b977eb10adf443b0109819b6182fc33
Eric Alves <eralves01@gmail.com>
2025-03-31 22:54:23 -0400

    feat: config database

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/eralves01/user-services/configs"
+	"github.com/eralves01/user-services/database"
 	"github.com/eralves01/user-services/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Rodando")
+	configs.LoadConfig()
+
+	// Criar instância do banco
+	db := database.NewDatabase().GetInstance()
+	defer database.NewDatabase().CloseConnection()
+
+	// Testar conexão
+	err := db.Ping()
+	if err != nil {
+		log.Fatalf("Database connection failed: %v", err)
+	} else {
+		log.Println("Database connected successfully!")
+	}
+
 	router := gin.Default()
 
-	routes.RegisterRoutes(router)
+	r := routes.NewRegisterRoutes(db, router)
+	r.GetRegisterRoutes()
 
 	http.ListenAndServe(":8080", router)
 }
